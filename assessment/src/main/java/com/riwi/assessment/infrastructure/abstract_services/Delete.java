91925580e29f42292a1cccaafdc679834c93a8e7
Santiago Echeverry <santiagocherrys@hotmail.com>
2024-07-29 10:45:43 -0500

    Create folder of generic and move create, delete...

--- assessment/src/main/java/com/riwi/assessment/infrastructure/abstract_services/Delete.java
+++
@@ -1,5 +0,0 @@
-package com.riwi.assessment.infrastructure.abstract_services;
-
-public interface Delete<ID>{
-    void delete(ID id);
-}
--- assessment/src/main/java/com/riwi/assessment/infrastructure/abstract_services/generic/GetById.java
+++ assessment/src/main/java/com/riwi/assessment/infrastructure/abstract_services/generic/GetById.java
@@ -1,4 +1,4 @@
-package com.riwi.assessment.infrastructure.abstract_services;
+package com.riwi.assessment.infrastructure.abstract_services.generic;
 
 public interface GetById<Response, ID>{
     Response getById(ID id);
