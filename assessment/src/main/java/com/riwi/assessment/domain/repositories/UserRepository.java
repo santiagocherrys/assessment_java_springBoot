2492cea5c4c4f4407b1f1b94b3324b015427ea69
Santiago Echevery Serna <santiagocherrys@hotmail.com>
2024-07-28 22:41:48 -0500

    Assessment only structure without sample

--- assessment/src/main/java/com/riwi/assessment/domain/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.riwi.assessment.domain.repositories;
-
-import com.riwi.assessment.domain.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-}
