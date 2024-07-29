61a682d3cbb6741569c5c91a9d7ac53c32919ad2
tejada <camilotejada1@gmail.com>
2024-07-29 11:44:37 -0500

    004-coupon-redeemed/fix/  error solutions

--- assessment/src/main/java/com/riwi/assessment/domain/entities/Coupon.java
+++ assessment/src/main/java/com/riwi/assessment/domain/entities/Coupon.java
@@ -2,22 +2,25 @@ package com.riwi.assessment.domain.entities;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Date;
+
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name = "coupons")
+@Entity(name = "coupons_entity")
 @Getter
 @Setter
 @Builder
@@ -25,16 +28,20 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Coupon {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
-    @Column(nullable = false, columnDefinition = "DECIMAL(0,2)")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
     private BigDecimal discount;
     @Column(nullable = false)
     private LocalDate expirationDate;
     @Column(nullable = false)
     private String state;
 
-    /*@ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId", referencedColumnName = "id")
-    private User user;*/
+    private Users user;
+    @OneToOne
+    @JoinColumn(name = "couponRedemed_id", referencedColumnName = "id")
+    private CouponReedemedEntity couponReedemedEntity;
+
 }
