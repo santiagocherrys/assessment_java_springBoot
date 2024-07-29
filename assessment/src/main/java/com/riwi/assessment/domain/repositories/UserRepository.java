dad43b2fdcf55ab1dedce1246a4219cfeba6a52f
David Osorio <daalosx@gmail.com>
2024-07-29 11:02:56 -0500

    creating the repository for user

---
+++ assessment/src/main/java/com/riwi/assessment/domain/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.riwi.assessment.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.riwi.assessment.domain.entities.Users;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+    
+}
