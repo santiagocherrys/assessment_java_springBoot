adcbccd536c81147a4ebf70e9cef625b6104fd5a
Santiago Echeverry <santiagocherrys@hotmail.com>
2024-07-29 11:01:22 -0500

    creation of DTO response, IProductService and ProductService

---
+++ assessment/src/main/java/com/riwi/assessment/infrastructure/services/ProductService.java
@@ -0,0 +1,10 @@
+package com.riwi.assessment.infrastructure.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ProductService {
+
+}
